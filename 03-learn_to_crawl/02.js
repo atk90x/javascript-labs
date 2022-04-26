452c5e037e6c96dcde3cc84e4be4ae3de48fac00
atk90x <alteralex@gmail.com>
2022-04-26 09:51:11 +0200

    Ejercicios hasta el 4

---
+++ 03-learn_to_crawl/02.js
@@ -0,0 +1,12 @@
+// Debug and modify the code so that the console does not display any errors.
+// Make the sum at the end add the apples and oranges.
+
+let tao = "unchanging"
+tao = "i've changed"
+
+let apples = 5
+let lemons = 12
+
+let fruit = apples + lemons
+
+console.log(fruit);
